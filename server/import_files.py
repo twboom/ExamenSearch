101d53e34b43e5323722ca42bcd33c67d68619d3
Thijs Boom <twboom@hotmail.nl>
2022-03-17 09:40:34 +0100

    Possible fix?

--- server/import_files.py
+++ server/import_files.py
@@ -18,7 +18,9 @@ def import_all():
                 files = get_files(subject[1])
                 for file in files:
                     period = file.split("/")[5].split("-")[1]
-                    document_type = file.split("/")[7].split("-")[0]
+                    document_type = file.split("/")[7].split("-")
+                    if len(document_type) > 1:
+                        document_type = document_type[0]
                     new_file(file, year, subject[0], level, period, document_type)
                     print(f"Imported {year} {level} {subject[0]} {period} {document_type}")
 
