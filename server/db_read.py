9b97189238fd68fb20966b7bf20aac126876b55f
Thijs Boom <twboom@hotmail.nl>
2022-03-17 21:36:43 +0100

    Made everything working

--- server/db_read.py
+++ server/db_read.py
@@ -19,23 +19,77 @@ with closing(sqlite3.connect(db_file)) as connection:
 
 
 # Search for a file
-def query(keywords=[], levels="*", subjects="*", years="*", document_types="*", periods="*") -> list:
+def query(keywords=[]) -> list:
     with closing(sqlite3.connect(db_file)) as connection:
         with closing(connection.cursor()) as cursor:
-            results = []
-            
+            query = "SELECT * FROM page"
+
+            data = []
             for keyword in keywords:
-                keyword = str(keyword).lower()
-                query = "SELECT * FROM page WHERE text_content LIKE ?"
-                data = (
-                    f"%{keyword}%",
-                )
+                data.append(f"%{keyword}%")
+                if keywords.index(keyword) == 0:
+                    query += " WHERE text_content LIKE ?"
+                    continue
+                query += " OR text_content LIKE ?"
+
+            data = tuple(data)
+            cursor.execute(query, data)
+            return cursor.fetchall()
+
+
+def get_files(page_list=[]):
+    with closing(sqlite3.connect(db_file)) as connection:
+        with closing(connection.cursor()) as cursor:
+            files = []
+            for page in page_list:
+                file_id = page[1]
+                query = "SELECT * FROM file WHERE id = ?"
+                data = (file_id,)
                 cursor.execute(query, data)
-                results += cursor.fetchall()
-            return results
+                files += cursor.fetchall()
+            return files
+
+
+def search(keywords=[]) -> list:
+    query_results = query(keywords=keywords)
+    files = get_files(page_list=query_results)
+
+    search_result = []
+    for qr in query_results:
+        file = files[query_results.index(qr)]
+        result = {
+            "page_number": qr[2] + 1,
+            "url": file[1],
+            "year": file[2],
+            "subject": file[3],
+            "level": file[4],
+            "period": file[5],
+            "document_type": file[6]
+        }
+        search_result.append(result)
+    
+    return search_result
+
+
+# Stats
+def get_total_files() -> int:
+    with closing(sqlite3.connect(db_file)) as connection:
+        with closing(connection.cursor()) as cursor:
+            query = "SELECT COUNT(url) FROM file"
+            cursor.execute(query)
+            return cursor.fetchone()[0]
+
+def get_total_pages() -> int:
+    with closing(sqlite3.connect(db_file)) as connection:
+        with closing(connection.cursor()) as cursor:
+            query = "SELECT COUNT(text_content) FROM page"
+            cursor.execute(query)
+            return cursor.fetchone()[0]
 
 
 if __name__ == "__main__":
-    result = query(keywords=["hart", "lees"])
-    # result = [result[1] for result in result]
-    print(len(result))
+    sr = search(keywords=["hartcyclus"])
+    print(len(sr))
+    print(sr[0])
+    # print(get_total_files())
+    # print(get_total_pages())
--- server/server.py
+++ server/server.py
@@ -2,26 +2,63 @@ import asyncio
 import websockets
 import json
 
+from db_read import search, get_total_files, get_total_pages
+
 
 async def send_message(websocket, data):
     data = json.dumps(data)
     await websocket.send(data)
 
 
-async def handle_message(websocket, path):
+async def handle_query(websocket, data):
+    data = data["data"]
+    if not "keywords in data":
+        send_message(websocket, {
+            "subject": "ERROR",
+            "data": "No keywords found on 'data' attribute"
+        })
+    
+    keywords = data["keywords"]
+    results = search(keywords=keywords)
+
+    await send_message(websocket, {
+        "subject": "QUERY_RESPONSE",
+        "data": {
+            "total_results": len(results),
+            "total_files": get_total_files(),
+            "total_pages": get_total_pages(),
+            "results": results,
+        }
+    })
+
+
+async def handle_message(websocket, message):
+        try:
+            data = json.loads(message)
+            if data["subject"] == "QUERY":
+                await handle_query(websocket, data)
+        except Exception as e:
+            print(e)
+            await send_message(websocket, {
+                "subject": "ERROR",
+                "data": {
+                    "message": "Exception occured while handling the message",
+                }
+            })
+
+
+async def handle_connection(websocket, path):
     data = {
         "subject": "CONNECTION",
         "data": "CONNECTED"
     }
     await send_message(websocket, data)
     async for message in websocket:
-        data = json.loads(message)
-        print(data)
-        await send_message(websocket, data)
+        await handle_message(websocket, message)
 
 
 async def main():
-    async with websockets.serve(handle_message, "localhost", 8080):
+    async with websockets.serve(handle_connection, "localhost", 8080):
         await asyncio.Future()
 
 
