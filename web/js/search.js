20d73675a4efa7da1f8e92fb4b07330d71c7f042
Thijs Boom <twboom@hotmail.nl>
2022-03-20 20:31:07 +0100

    Added support for multiple keywords (OR)

--- web/js/search.js
+++ web/js/search.js
@@ -3,14 +3,15 @@ const button = document.getElementById('search-button');
 
 function search() {
     if (input.value.length == '') { return };
-    let keywords = [input.value];
+    let keywords = input.value.split(';');
+    keywords = keywords.map(keyword => keyword.trim());
     sendQuery(keywords);
     console.log('CLIENT:', `Searching for "${keywords}"`);
-}
+};
 
-button.addEventListener("click", search)
+button.addEventListener("click", search);
 input.addEventListener("keypress", evt => {
     if (evt.key === 'Enter') {
         search();
-    }
-})
+    };
+});
